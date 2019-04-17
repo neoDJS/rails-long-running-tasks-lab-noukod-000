1e745e042f684acabe30457a6a3fcfc7f00ed17c
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-17 10:43:50 +0000

    Automatically backed up by Learn

--- db/migrate/20160108015311_create_songs.rb
+++ db/migrate/20160108015311_create_songs.rb
@@ -2,7 +2,6 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :title
-      t.integer :release_year
 
       t.timestamps null: false
     end
